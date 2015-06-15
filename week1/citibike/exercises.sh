629d9480fd1f8e0ec9dcfd9f8dddef5c787137f2
Jake Hofman <jhofman@gmail.com>
2015-06-15 16:48:17 -0400

    citibike examples

---
+++ week1/citibike/exercises.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# add your solution after each of the 10 comments below
+#
+
+# count the number of unique stations
+
+# count the number of unique bikes
+
+# extract all of the trip start times
+
+# count the number of trips per day
+
+# find the day with the most rides
+
+# find the day with the fewest rides
+
+# find the id of the bike with the most rides
+
+# count the number of riders by gender and birth year
+
+# count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
+
+# compute the average trip duraction
