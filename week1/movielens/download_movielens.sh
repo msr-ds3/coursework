e65e6a8664ac546358395414b45f9da110745b83
Jake Hofman <jhofman@gmail.com>
2015-06-17 10:40:18 -0400

    movielens

---
+++ week1/movielens/download_movielens.sh
@@ -0,0 +1,29 @@
+'1;2c#!/bin/bash
+#
+# downloads movielens rating data
+#
+# see http://grouplens.org/datasets/movielens/ for more info
+#
+
+DATA_DIR=.
+
+url=http://files.grouplens.org/datasets/movielens/ml-10m.zip
+
+# create movielens directory
+[ -d $DATA_DIR/movielens ] || mkdir -p $DATA_DIR/movielens
+
+# change to movielens directory
+cd $DATA_DIR/movielens
+
+# download ratings zip file
+[ -f movielens_10M.zip ] || curl -o movielens_10M.zip $url
+
+# uncompress zip file
+if [ ! -f ratings.dat ]
+    then
+    unzip movielens_10M.zip && mv ml-10M100K/* .
+fi
+
+# reformat to comma-separated file
+[ -f ratings.csv ] || cat ratings.dat | sed 's/::/,/g' > ratings.csv
+
