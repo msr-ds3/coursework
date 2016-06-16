595138b4d211a129f84ef827d8a54efae0b368e8
Jake Hofman <jhofman@gmail.com>
2016-06-16 10:08:33 -0400

    small tweaks to movielens exercises

--- week1/movielens/movielens.R
+++ week1/movielens/movielens.R
@@ -1,18 +1,21 @@
 library(dplyr)    # install.packages('dplyr')
 library(ggplot2)  # install.packages('ggplot2')
 library(scales)   # install.packages('scales')
+library(readr)    # install.packages('readr')
 
 # set plot theme
 theme_set(theme_bw())
 
 # read ratings from csv file
-system.time(
-  ratings <- read.delim('ratings.csv',
-                        sep=',',
-			header=F,
-                        col.names=c('user_id','movie_id','rating','timestamp'),
-                        colClasses=c('integer','integer','numeric','integer'))
-)
+ratings <- read_csv('ratings.csv')
+
+# for reference: same thing, using base R functions and explicitly setting column information
+#   ratings <- read.delim('ratings.csv',
+#                         sep=',',
+#                         header=F,
+#                         col.names=c('user_id','movie_id','rating','timestamp'),
+#                         colClasses=c('integer','integer','numeric','integer'))
+
 print(object.size(ratings), units="Mb")
 
 ####################
@@ -30,7 +33,7 @@ str(ratings)
 # compute aggregate stats
 summary(ratings$rating)
 
-# plot distribution of ratings (slide 13)
+# plot distribution of rating values (slide 13)
 
 
 ####################
@@ -38,14 +41,18 @@ summary(ratings$rating)
 ####################
 
 # aggregate ratings by movie, computing mean and number of ratings
+# hint: use the n() function for easy counting within a group
 
 # plot distribution of movie popularity (= number of ratings the movie received)
+# hint: try scale_x_log10() for a logarithmic x axis
 
 # plot distribution of mean ratings by movie (slide 15)
+# hint: try geom_histogram and geom_density
 
 # rank movies by popularity and compute cdf (slide 17)
-# hint: use the cumsum function for a running sum
+# hint: use dplyr's rank and arrange functions, and the base R sum and cumsum functions
 
+# plot the CDF of movie popularity
 
 ####################
 # per-user stats
@@ -54,3 +61,4 @@ summary(ratings$rating)
 # aggregate ratings by user, computing mean and number of ratings
 
 # plot distribution of user activity (= number of ratings the user made)
+# hint: try a log scale here
