1132411b459121b4e9f5b85565bd81b772e0e02b
Jake Hofman <jhofman@gmail.com>
2016-06-17 13:33:08 -0400

    few more citibike plotting exercises

--- week1/citibike/plot_trips.R
+++ week1/citibike/plot_trips.R
@@ -7,6 +7,8 @@ library(dplyr)
 library(ggplot2)
 library(reshape)
 library(scales)
+library(tidyr)
+library(lubridate)
 
 # be picky about white backgrounds on our plots
 theme_set(theme_bw())
@@ -27,14 +29,23 @@ load('trips.RData')
 
 # plot the number of trips by gender and age
 
+# plot the ratio of male to female trips by age
+# hint: use the spread() function to reshape things to make it easier to compute this ratio
+
+########################################
+# plot weather data
+########################################
+# plot the minimum temperature over each day
+
+# plot the minimum temperature and maximum temperature over each day
+# hint: try using the gather() function for this to reshape things before plotting
+
 ########################################
 # plot trip and weather data
 ########################################
 
 # join trips and weather
 trips_with_weather <- inner_join(trips, weather, by="ymd")
-
-# plot the minimum temperature over each day
 
 # plot the number of trips as a function of the minimum temperature, where each point represents a day
 # you'll need to summarize the trips and join to the weather data to do this
@@ -43,3 +54,10 @@ trips_with_weather <- inner_join(trips, weather, by="ymd")
 # you'll need to decide what constitutes "substantial precipitation" and create a new T/F column to indicate this
 
 # add a smoothed fit on top of the previous plot, using geom_smooth
+
+# compute the average number of trips and standard deviation in number of trips by hour of the day
+# hint: use the hour() function from the lubridate package
+
+# plot the above
+
+# repeat this, but now split the results by day of the week (Monday, Tuesday, ...) or weekday vs. weekend days
