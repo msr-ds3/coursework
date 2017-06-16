36a79647769ab70346f44b7064b71fdfd34cfca9
Jake Hofman <jhofman@gmail.com>
2017-06-16 14:37:33 -0400

    date fix

--- week1/students/musical_pairs.sh
+++ week1/students/musical_pairs.sh
@@ -1,1 +1,1 @@
-ls *.txt | awk 'BEGIN {srand("date +%Y%m%d")} {print rand(), $1;}' | sort | awk '{print $2; if (NR % 2 == 0) print "----------"}'
+ls *.txt | awk -v date=`date +%Y%m%d` 'BEGIN {srand(date)} {print rand(), $1;}' | sort | awk '{print $2; if (NR % 2 == 0) print "----------"}'
