b0ed4758d29d6fcc6bcea55512384fafcf26d703
Anthony Vallejo <hectorvallejo@Anthonys-Mac.local>
2021-06-02 16:49:28 -0400

    anthony vallejo

--- week1/citibike.sh
+++ week1/citibike.sh
@@ -5,19 +5,37 @@
 
 # count the number of unique stations
 
+cut -d, -f5 201402-citibike-tripdata.csv | sort | uniq -c | cat -n
+
 # count the number of unique bikes
+
+cut -d, -f12 201402-citibike-tripdata.csv | sort | uniq -c | cat -n
 
 # count the number of trips per day
 
+cut -d, -f2 201402-citibike-tripdata.csv| sort | cut -d" "  -f1 |sort |  uniq -c          
+
 # find the day with the most rides
+
+cut -d, -f2 201402-citibike-tripdata.csv| sort | cut -d" "  -f1 |sort |  uniq -c | sort -nr | head -n1
 
 # find the day with the fewest rides
 
+cut -d, -f2 201402-citibike-tripdata.csv| sort | cut -d" "  -f1 |sort |  uniq -c | sort -n | head -n2
+
 # find the id of the bike with the most rides
+
+cut -d, -f12 201402-citibike-tripdata.csv| sort | cut -d" "  -f1 |sort |  uniq -c | sort -nr  | head -n1
 
 # count the number of rides by gender and birth year
 
+cut -d, -f14,15 201402-citibike-tripdata.csv | grep '[0-9]' | sort | uniq -c | sort -nr
+
+
 # count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
 
+awk -F, '$5 ~ /1 Ave & E 15 St/' 201402-citibike-tripdata.csv | wc -l
 
 # compute the average trip duration
+
+
