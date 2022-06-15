d237b8aceeadf6310f277d384f1e7586b94bd966
cstewart27 <cstewart3@student.sjcny.edu>
2022-06-15 19:16:37 -0400

    week 3 day 3 exercises

--- week3/ngrams/02_filter_1grams.sh
+++ week3/ngrams/02_filter_1grams.sh
@@ -4,3 +4,7 @@
 #   decompress the first using gunzip, zless, zcat or similar
 #   then filter out rows that match using grep -E, egrep, awk, or similar
 #   write results to year_counts.tsv
+
+gunzip googlebooks-eng-all-1gram-20120701-1.gz
+
+grep -E '^(18|19|20)[0-9][0-9]' googlebooks-eng-all-1gram-20120701-1 > year_counts.tsv
--- week3/ngrams/04_reformat_totals.sh
+++ week3/ngrams/04_reformat_totals.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/bash\
 
 # reformat total counts in googlebooks-eng-all-totalcounts-20120701.txt to a valid csv
 #   use tr, awk, or sed to convert tabs to newlines
 #   write results to total_counts.csv
+tr -s '\t' '\n' < googlebooks-eng-all-totalcounts-20120701.txt > total_counts.csv
