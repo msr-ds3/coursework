c2f63f8f95632f77a675d9d8cdaa3bc925823285
root <root@warrenball-surfacebk3.localdomain>
2022-06-15 18:06:35 -0400

    warrens week 3 day 3

--- week3/ngrams/04_reformat_totals.sh
+++ week3/ngrams/04_reformat_totals.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-
-# reformat total counts in googlebooks-eng-all-totalcounts-20120701.txt to a valid csv
-#   use tr, awk, or sed to convert tabs to newlines
-#   write results to total_counts.csv
+tr '\t' '\n' < googlebooks-eng-all-totalcounts-20120701.txt | tail -n +2 > total_counts.csv
