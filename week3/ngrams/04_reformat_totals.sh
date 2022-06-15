bab08e2b85b973feaa7b46f13a102d6837c68e7a
root <root@sushobhan-surfacebk3.localdomain>
2022-06-15 17:49:07 -0400

    exercise

--- week3/ngrams/04_reformat_totals.sh
+++ week3/ngrams/04_reformat_totals.sh
@@ -3,3 +3,4 @@
 # reformat total counts in googlebooks-eng-all-totalcounts-20120701.txt to a valid csv
 #   use tr, awk, or sed to convert tabs to newlines
 #   write results to total_counts.csv
+tr -s '\t' '\n' < googlebooks-eng-all-totalcounts-20120701.txt > total_counts.csv
