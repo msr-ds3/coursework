addb6cbefd3bb0ef88b7ebeeb152dee9ff5ca820
ChristopherE-crypto <christopher.esquivel@lc.cuny.edu>
2022-06-16 08:06:36 -0400

    week3_day3_exercises_ngrams

--- week3/ngrams/01_download_1grams.sh
+++ week3/ngrams/01_download_1grams.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-
+#
 # use curl or wget to download the version 2 1gram file with all terms starting with "1", googlebooks-eng-all-1gram-20120701-1.gz
-DATA_DIR=.
-
-cd $DATA_DIR
-
-url=http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-3gram-20120701-1.gz
-curl -0 $url
-
-file=`basename $url`
-csv=${file//.gz/}".csv"
-
-unzip -p $file > $csv
-
-rm $file
-
+curl -O http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-1gram-20120701-1.gz
 # update the timestamp on the resulting file using touch
 # do not remove, this will keep make happy and avoid re-downloading of the data once you have it
 touch googlebooks-eng-all-1gram-20120701-1.gz
--- week3/ngrams/04_reformat_totals.sh
+++ week3/ngrams/04_reformat_totals.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
 # reformat total counts in googlebooks-eng-all-totalcounts-20120701.txt to a valid csv
+tr '\t' '\n' < googlebooks-eng-all-totalcounts-20120701.txt > total_counts.csv
 #   use tr, awk, or sed to convert tabs to newlines
 #   write results to total_counts.csv
