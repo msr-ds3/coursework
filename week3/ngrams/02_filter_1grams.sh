2630cd19dcd0f7a37d56854bcd1974ace84cef35
root <root@navpreet-surfacebook3.localdomain>
2022-06-15 20:18:05 -0400

    commit

--- week3/ngrams/02_filter_1grams.sh
+++ week3/ngrams/02_filter_1grams.sh
@@ -4,3 +4,4 @@
 #   decompress the first using gunzip, zless, zcat or similar
 #   then filter out rows that match using grep -E, egrep, awk, or similar
 #   write results to year_counts.tsv
+cat googlebooks-eng-all-1gram-20120701-1.gz | grep -E '^(18|19)[0-9][0-9][[:space:]]' > year_counts.tsv 
