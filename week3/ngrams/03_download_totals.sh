560809340df7de67ec80291a903acfdf2d1d31ad
limorko <limorkohanim15@gmail.com>
2022-06-15 20:15:55 -0400

    limor's ngrams

--- week3/ngrams/03_download_totals.sh
+++ week3/ngrams/03_download_totals.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 # use curl or wget to download the version 2 of the total counts file, googlebooks-eng-all-totalcounts-20120701.txt
-
+curl http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-totalcounts-20120701.txt --output googlebooks-eng-all-totalcounts-20120701.txt
 # update the timestamp on the resulting file using touch
 # do not remove, this will keep make happy and avoid re-downloading of the data once you have it
 touch googlebooks-eng-all-totalcounts-20120701.txt
