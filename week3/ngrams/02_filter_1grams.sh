24d07ad22e29d2521a70eaab68d67a8797a95a4b
root <root@dannyg.localdomain>
2022-06-16 10:08:11 -0400

    yesterday day 3

--- week3/ngrams/02_filter_1grams.sh
+++ week3/ngrams/02_filter_1grams.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 # filter original 1gram file googlebooks-eng-all-1gram-20120701-1.gz to only lines where the ngram exactly matches a year (18xx, 19xx, or 20xx, where x is a digit)
 #   decompress the first using gunzip, zless, zcat or similar
+#sudo apt install dos2unix
+#dos2unix 01_download_1grams.sh
+#gunzip googlebooks-eng-all-1gram-20120701-1.gz
 #   then filter out rows that match using grep -E, egrep, awk, or similar
-#   write results to year_counts.tsv
+#   write results to year_counts.tsv 
+grep -E "^(18|19|20)[0-9][0-9]" googlebooks-eng-all-1gram-20120701-1  > year_counts.tsv
