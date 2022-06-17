ffccc914abc64707a4d7226e64aecc75938396ea
nirvi8 <106620114+nirvi8@users.noreply.github.com>
2022-06-16 22:22:36 -0400

    updates

--- week3/ngrams/04_reformat_totals.sh
+++ week3/ngrams/04_reformat_totals.sh
@@ -3,3 +3,4 @@
 # reformat total counts in googlebooks-eng-all-totalcounts-20120701.txt to a valid csv
 #   use tr, awk, or sed to convert tabs to newlines
 #   write results to total_counts.csv
+cat googlebooks-eng-all-totalcounts-20120701.txt | tr "\t" "\n" >> total_counts.csv 
