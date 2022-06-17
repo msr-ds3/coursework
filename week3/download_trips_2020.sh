4bececb2b6176c1527cece37b57beaff981bd03f
root <root@dannyg.localdomain>
2022-06-17 17:04:00 -0400

    Files from week3 day 5. Still need to swap models.

---
+++ week3/download_trips_2020.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# description:
+#   fetches trip files from the citibike site http://www.citibikenyc.com/system-data
+#   e.g., https://s3.amazonaws.com/tripdata/201307-citibike-tripdata.zip
+#
+# usage: ./download_trips.sh
+#
+# requirements: curl or wget
+#
+# author: jake hofman
+#
+
+# set a relative path for the citibike data
+# (use current directory by default)
+DATA_DIR=.
+
+# change to the data directory
+cd $DATA_DIR
+
+# loop over each year/month
+for year in 2020
+do
+    # note: remove '#' in line below to download all months
+    for month in 01 02 03 04 05 06 07 08 09 10 11 12
+    do
+
+	# download the zip file
+	# alternatively you can use wget if you don't have curl
+	# wget $url
+	url=https://s3.amazonaws.com/tripdata/${year}${month}-citibike-tripdata.csv.zip
+	curl -O $url
+
+	# define local file names
+	file=`basename $url`
+	csv=${file//.zip/}
+	
+	# unzip the downloaded file
+	unzip -p $file > $csv
+
+	# remove the zip file
+	rm $file
+    done
+done
+#201401-citibike-tripdata.zip
+#202001-citibike-tripdata.csv.zip
