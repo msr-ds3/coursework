c296aefa46dd83d14cdc69629ee135e044b63c6e
root <root@idi>
2023-05-31 05:52:24 -0400

    Updating the files

--- week1/citibike.sh
+++ week1/citibike.sh
@@ -4,14 +4,22 @@
 #
 
 # count the number of unique stations
-
+  tail -n +2 201402-citibike-tripdata.csv | cut -d, -f4,8  | tr , "\n" | sort | uniq |
+ wc -l
 # count the number of unique bikes
+ tail -n +2 201402-citibike-tripdata.csv | cut -d, -f12 | sort |uniq | wc -l 
 
 # count the number of trips per day
+cut -d, -f2 201402-citibike-tripdata.csv  | cut -d" " -f1 | sort | uniq
+ -c
 
 # find the day with the most rides
+cut -d, -f2 201402-citibike-tripdata.csv  | cut -d" " -f1 | sort | uniq -c | sort -r| head -n1 | cut -d\" -f2
+
 
 # find the day with the fewest rides
+ tail -n +2 201402-citibike-tripdata.csv |cut -d, -f2 | cut -d" " -f1 |
+sort | uniq -c | sort | head -n1 | cut -d\" -f2
 
 # find the id of the bike with the most rides
 
