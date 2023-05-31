4b3845464399cab12af371505350e439a2f90f7f
root <root@idi>
2023-05-31 06:21:17 -0400

    citibike.sh changes

--- week1/citibike.sh
+++ week1/citibike.sh
@@ -22,10 +22,14 @@ cut -d, -f2 201402-citibike-tripdata.csv  | cut -d" " -f1 | sort | uniq -c | sor
 sort | uniq -c | sort | head -n1 | cut -d\" -f2
 
 # find the id of the bike with the most rides
+tail -n +2 201402-citibike-tripdata.csv |cut -d, -f12 | sort | uniq -c | sort -r | head -n1 | cut -d\" -f2
 
 # count the number of rides by gender and birth year
+tail -n +2 201402-citibike-tripdata.csv | cut -d, -f15,14 | sort | uniq -c | sort
 
 # count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
+cut -d, -f5 201402-citibike-tripdata.csv | grep '.*[0-9].* & .*[0-9].*' | wc -l
 
 
 # compute the average trip duration
+tail -n +2 201402-citibike-tripdata.csv | cut -d, -f1 | tr -d \" | awk '{count+=$1} END {print count/NR}'
