96030f27005ff01fa79809b45eafc18308b56900
root <root@xzhu>
2023-05-31 17:25:54 -0400

    DONE

--- week1/citibike.sh
+++ week1/citibike.sh
@@ -4,20 +4,30 @@
 #
 
 # count the number of unique stations
+cut -d, -f5 201402-citibike-tripdata.csv | sort | uniq | wc -l
 
 # count the number of unique bikes
+cut -d, -f12 201402-citibike-tripdata.csv | sort | uniq | wc -l
 
 # count the number of trips per day
-
+cut -d, -f2 201402-citibike-tripdata.csv | cut -c 2-11 | sort | uniq -c
 # find the day with the most rides
+ cut -d, -f2 201402-citibike-tripdata.csv | cut -d" " -f1 | tr -d '"' | sort | uniq -c | sort | tail -n1  
+ 
+ cut -d, -f2 201402-citibike-tripdata.csv | cut -c 2-11 | sort | uniq -c | sort | tail -n1
 
 # find the day with the fewest rides
-
+ cut -d, -f2 201402-citibike-tripdata.csv | cut -d" " -f1 | tr -d '"' | sort | uniq -c | sort -r| tail -n2  
+ cut -d, -f2 201402-citibike-tripdata.csv | cut -c 2-11 | sort | uniq -c | sort -r| tail -n2
 # find the id of the bike with the most rides
-
+ cut -d, -f12 201402-citibike-tripd
+ata.csv | sort | uniq -c | sort | tail -n1
 # count the number of rides by gender and birth year
-
+ cut -d, -f14,15 201402-citib
+ike-tripdata.csv |sort | uniq -c
 # count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
-
+cut -d, -f5 201402-citibike-tripdata.csv | wc -l
 
 # compute the average trip duration
+
+
