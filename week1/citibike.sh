1de68bd960c25472940d39b0bc1f58f485db19b0
Elvis74751 <127332555+Elvis74751@users.noreply.github.com>
2023-05-31 17:27:01 -0400

    All questions done except last one

--- week1/citibike.sh
+++ week1/citibike.sh
@@ -4,20 +4,34 @@
 #
 
 # count the number of unique stations
-
+cut -d, -f5 201402-citibike-tripdata.csv | sort | uniq | wc -l
 # count the number of unique bikes
-
+cut -d, -f12 201402-citibike-tripdata.csv | sort | uniq | wc -l
 # count the number of trips per day
+
+cut -d, -f2 201402-citibike-tripdata.csv | cut -c 2-11 | sort | uniq -c
 
 # find the day with the most rides
 
+cut -d, -f2 201402-citibike-tripdata.csv | cut -c 2-11 | sort | uniq -c | sort | tail -n1 
+
 # find the day with the fewest rides
 
-# find the id of the bike with the most rides
+cut -d, -f2 201402-citibike-tripdata.csv | cut -c 2-11 | sort | uniq -c | sort | head -n2
+
+# find the id of the bike with the most ride
+
+cut -d, -f12 201402-citibike-tripdata.csv | sort | uniq -c | sort | tail -n1
 
 # count the number of rides by gender and birth year
 
+cut -d, -f15,14 201402-citibike-tripdata.csv | sort | uniq -c
+
 # count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
 
+cut -d, -f5 201402-citibike-tripdata.csv | grep '[0-9].*&.*[0-9]' | wc -l
 
 # compute the average trip duration
+
+
+
