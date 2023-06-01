e99f81a82589e20aa93c4c3d2cd120f207dba8a5
root <root@ballo>
2023-06-01 00:05:41 -0400

    Command line practice

--- week1/citibike.sh
+++ week1/citibike.sh
@@ -4,17 +4,21 @@
 #
 
 # count the number of unique stations
+cut -d, -f4,8 201402-citibike-tripdata.csv | tr ',' '\n' | sort | uniq | wc -l
 
 # count the number of unique bikes
+cut -d, -f12 201402-citibike-tripdata.csv | tr ',' '\n' | sort | uniq | wc -l
 
 # count the number of trips per day
-
+cut -d, -f4,8 201402-citibike-tripdata.csv | tr ',' '\n' | sort | uniq | wc -l
+# create a numbered list of columns, translating commas to newlines and adding line numbers
+awk -F, '2014-02-28' 201402-citibike-tripdata.csv | wc -l
 # find the day with the most rides
-
+cut -d, -f2 201402-citibike-tripdata.csv | cut -d ' ' -f1 | sort | uniq -c | sort | tail -n1
 # find the day with the fewest rides
-
+cut -d, -f2 201402-citibike-tripdata.csv | cut -d ' ' -f1 | sort | uniq -c | sort | head -n2 | tail -n1
 # find the id of the bike with the most rides
-
+cut -d, -f12 201402-citibike-tripdata.csv | sort | uniq -c | sort | tail -n1
 # count the number of rides by gender and birth year
 
 # count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
