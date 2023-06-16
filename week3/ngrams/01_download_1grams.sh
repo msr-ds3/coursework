f1d25d698ec7cc9a3aa186835d8a0473a705d749
alexkhuey <alexkhuey@gmail.com>
2023-06-16 14:02:22 -0400

    update

--- week3/ngrams/01_download_1grams.sh
+++ week3/ngrams/01_download_1grams.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # use curl or wget to download the version 2 1gram file with all terms starting with "1", googlebooks-eng-all-1gram-20120701-1.gz
-[ -f googlebooks-eng-all-1gram-20120701-1.gz ] || curl -o googlebooks-eng-all-1gram-20120701-1.gz http://storage.googleapis.com/books/ngrams/books/datasetsv2.html
+curl -O http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-1gram-20120701-1.gz 
 # update the timestamp on the resulting file using touch
 # do not remove, this will keep make happy and avoid re-downloading of the data once you have it
 touch googlebooks-eng-all-1gram-20120701-1.gz
