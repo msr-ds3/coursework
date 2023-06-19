3cb554a5b00807108e9483c27fe4afe0ac1e1c06
root <root@goldberger>
2023-06-19 00:53:56 -0400

    Completed week3/ngrams 01 to 05 files

--- week3/ngrams/03_download_totals.sh
+++ week3/ngrams/03_download_totals.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # use curl or wget to download the version 2 of the total counts file, googlebooks-eng-all-totalcounts-20120701.txt
+url=http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-totalcounts-20120701.txt
+curl -O $url
 
 # update the timestamp on the resulting file using touch
 # do not remove, this will keep make happy and avoid re-downloading of the data once you have it
