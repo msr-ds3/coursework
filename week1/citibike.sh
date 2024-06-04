84df6f2e9a58e96ea576d5031c5246572b2365e7
root <root@shimmy-surface>
2024-06-04 14:10:34 -0400

    Removed the TODO line after checking in with Jenny.

--- week1/citibike.sh
+++ week1/citibike.sh
@@ -13,7 +13,6 @@ cut -d, -f2 201402* | cut -c1-10 | sort | uniq -c
 cut -d, -f2 201402* | cut -c1-10 | sort | uniq -c | sort -n | tail -n1
 # find the day with the fewest rides
 cut -d, -f2 201402* | cut -c1-10 | sort | uniq -c | sort -n | head -n1
-# TODO: Figure out if it's a problem that this yields starttime
 # find the id of the bike with the most rides
 cut -d, -f12 201402* | sort | uniq -c | sort -n | tail -n1
 # count the number of rides by gender and birth year
