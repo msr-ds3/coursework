458a06ff2242af6be617c2699f6dd57ba980d8d7
root <root@matt-surface>
2024-06-04 16:18:34 -0400

    day 2 assignment

--- week1/citibike.sh
+++ week1/citibike.sh
@@ -4,20 +4,21 @@
 #
 
 # count the number of unique stations
-
+cut -d, -f4 201402* | sort | uniq -c | wc -l 
 # count the number of unique bikes
-
+cut -d, -f4 20102* | sort | uniq -c | wc -l
 # count the number of trips per day
-
+cut -d, -f4 201402* | cut -c-10 | uniq -c
 # find the day with the most rides
-
+cut -d, -f4 201402* | cut -c-10 | uniq -c | sort -r | head -n2
 # find the day with the fewest rides
-
+cut -d, -f4 201402* | cut -c-10 | uniq -c | sort | head -n2
 # find the id of the bike with the most rides
-
+cut -d,f12 201402* | sort | uniq -c | tail -n
 # count the number of rides by gender and birth year
-
+ut -d, -f15,14 201402* | sort | uniq -c
 # count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
-
+cut -d, -f5 201402* | grep '[0-9].*[^0-9].*[0-9]' | wc -l
 
 # compute the average trip duration
+awk '{total += $1;freq += 1} END {print total/freq}' 201402*
--- week1/download_trips.sh
+++ week1/download_trips.sh
@@ -9,27 +9,21 @@
 #
 # author: jake hofman
 #
-
 # set a relative path for the citibike data
 # (use current directory by default)
 DATA_DIR=.
-
 # change to the data directory
 cd $DATA_DIR
-
 # loop over each year/month
 for year in 2014
 do
-
     # download the zip file
     # alternatively you can use wget if you don't have curl
     # wget $url
     url=https://s3.amazonaws.com/tripdata/${year}-citibike-tripdata.zip
     curl -O $url
-
     # unzip the year's data
     unzip ${year}-citibike-tripdata.zip
-
     # move files out of subdirectories to current directory
     # getting rid of the annoying _1 in the filenames
     for f in ${year}-citibike-tripdata/*/*.csv
@@ -37,7 +31,6 @@ do
 	new_file=`basename ${f//_1/}`
 	mv $f $new_file
     done
-
     # remove the zip file
     rm ${year}-citibike-tripdata.zip
 done
