a7933df290f547e38b1a8568015a6b690fd031e4
root <root@matt-surface>
2024-06-05 00:46:48 -0400

    day 2 assignment fixes

--- week1/citibike.sh
+++ week1/citibike.sh
@@ -6,9 +6,9 @@
 # count the number of unique stations
 cut -d, -f4 201402* | sort | uniq -c | wc -l 
 # count the number of unique bikes
-cut -d, -f4 20102* | sort | uniq -c | wc -l
+cut -d, -f12 20102* | sort | uniq -c | wc -l
 # count the number of trips per day
-cut -d, -f4 201402* | cut -c-10 | uniq -c
+ut -d, -f2 201402* | cut -c-10 | uniq -c | tail -n+2
 # find the day with the most rides
 cut -d, -f4 201402* | cut -c-10 | uniq -c | sort -r | head -n2
 # find the day with the fewest rides
@@ -16,7 +16,7 @@ cut -d, -f4 201402* | cut -c-10 | uniq -c | sort | head -n2
 # find the id of the bike with the most rides
 cut -d,f12 201402* | sort | uniq -c | tail -n
 # count the number of rides by gender and birth year
-ut -d, -f15,14 201402* | sort | uniq -c
+cut -d, -f15,14 201402* | sort | uniq -c
 # count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
 cut -d, -f5 201402* | grep '[0-9].*[^0-9].*[0-9]' | wc -l
 
