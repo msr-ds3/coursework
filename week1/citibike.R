bc049810f5afea556d99fa990873d8e586a7f97e
phuongjanele <lengocphuong26701@gmail.com>
2024-06-05 17:44:28 -0400

    Update citibike.R

--- week1/citibike.R
+++ week1/citibike.R
@@ -6,7 +6,7 @@ library(lubridate)
 ########################################
 
 # read one month of data
-trips <- read_csv('201402-citibike-tripdata.csv')
+trips <- read_csv('C:/Users/jane/Documents/Github/coursework/week1/201402-citibike-tripdata.csv')
 
 # replace spaces in column names with underscores
 names(trips) <- gsub(' ', '_', names(trips))
@@ -24,25 +24,83 @@ trips <- mutate(trips, gender = factor(gender, levels=c(0,1,2), labels = c("Unkn
 
 # count the number of trips (= rows in the data frame)
 
-# find the earliest and latest birth years (see help for max and min to deal with NAs)
+nrow(trips)
+
+trips %>%
+  mutate(birth_year = ifelse(birth_year == "\\N", NA, birth_year)) %>%
+  filter(!is.na(as.numeric(birth_year))) %>%
+  summarize(earliest_birth_year = min(as.numeric(birth_year)), 
+            latest_birth_year = max(as.numeric(birth_year)))%>%View()
+
+#mutate(formatted_birth_yer = as.numeric(birth_year))%>%
 
 # use filter and grepl to find all trips that either start or end on broadway
+trips%>%
+  filter(grepl('^Broadway', start_station_name) | grepl('^Broadway', end_station_name))%>%View()
+
 
 # do the same, but find all trips that both start and end on broadway
+trips%>%
+  filter(grepl('^Broadway', start_station_name), grepl('^Broadway', end_station_name))%>%View()
 
 # find all unique station names
+
+trips%>%
+  select(start_station_name)%>%
+  unique()%>%View()
 
 # count the number of trips by gender, the average trip time by gender, and the standard deviation in trip time by gender
 # do this all at once, by using summarize() with multiple arguments
 
+trips %>%
+  group_by(gender) %>%
+  summarize(sd_by_gender = sd(tripduration)) %>%
+  View()
+
+
 # find the 10 most frequent station-to-station trips
+trips%>%
+  group_by(start_station_name,end_station_name)%>%
+  summarize(count = n())%>%
+  arrange(desc(count))%>%
+  head(10)%>%View()
 
 # find the top 3 end stations for trips starting from each start station
+trips%>%
+  group_by(start_station_name, end_station_name)%>%
+  summarize(count = n())%>%
+  arrange(desc(count), by_group = TRUE)%>%
+  slice(1:3)%>%View()
+
+      
 
 # find the top 3 most common station-to-station trips by gender
+trips%>%
+  group_by(gender, start_station_name, end_station_name)%>%
+  summarize(count = n())%>%
+  group_by(gender)%>%
+  arrange(desc(count))%>%
+  slice(1:3)%>%View()
 
 # find the day with the most trips
 # tip: first add a column for year/month/day without time of day (use as.Date or floor_date from the lubridate package)
+trips%>%
+  mutate(date = as.Date(starttime))%>%
+  group_by(date)%>%
+  summarize(count = n())%>%
+  arrange(desc(count))%>%
+  slice(1)%>%View()
 
 # compute the average number of trips taken during each of the 24 hours of the day across the entire month
+trips%>%
+  mutate(hour = hour(starttime))%>%
+  group_by(hour)%>%
+  summarize(avg_trip = n()/n_distinct(as.Date(starttime)))%>%View()
+
+
 # what time(s) of day tend to be peak hour(s)?
+trips%>%
+  mutate(hour = hour(starttime))%>%
+  group_by(hour)%>%
+  summarize(avg_trip = n()/n_distinct(as.Date(starttime)))%>%
+  arrange(desc(avg_trip))%>%View()
