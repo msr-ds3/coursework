80fd370d7ca9d4243d46f3cddb2377e7bd34fdab
YinoXu <yx2347@nyu.edu>
2024-06-05 15:36:24 -0400

    citibike.c finished

--- week1/citibike.R
+++ week1/citibike.R
@@ -17,32 +17,70 @@ names(trips) <- gsub(' ', '_', names(trips))
 # recode gender as a factor 0->"Unknown", 1->"Male", 2->"Female"
 trips <- mutate(trips, gender = factor(gender, levels=c(0,1,2), labels = c("Unknown","Male","Female")))
 
-
 ########################################
 # YOUR SOLUTIONS BELOW
 ########################################
 
 # count the number of trips (= rows in the data frame)
+nrow(trips)
+# 224736
 
 # find the earliest and latest birth years (see help for max and min to deal with NAs)
+trips%>%
+  summarise(max_birth_year = max(as.numeric(birth_year), na.rm = TURE)) # The latest birth year : 1997
+trips%>%
+  summarise(trips, min_birth_year = min(as.numeric(birth_year), na.rm = TRUE)) # The earlist birth year : 1899
 
 # use filter and grepl to find all trips that either start or end on broadway
+trips %>%
+  filter(grepl('^Broadway', start_station_name) | grepl('^Broadway', end_station_name))
 
 # do the same, but find all trips that both start and end on broadway
+trips %>% 
+  filter(grepl('^Broadway', start_station_name) & grepl('^Broadway', end_station_name))
 
 # find all unique station names
+trips %>% 
+  distinct(start_station_name)
 
 # count the number of trips by gender, the average trip time by gender, and the standard deviation in trip time by gender
 # do this all at once, by using summarize() with multiple arguments
+trips %>% 
+  group_by(gender) %>% summarize(count = n(),mean(tripduration), sd(tripduration))
 
 # find the 10 most frequent station-to-station trips
+trips %>% 
+  group_by(start_station_name, end_station_name) %>% summarize(count = n()) %>% arrange(desc(count)) %>% head(10)
 
 # find the top 3 end stations for trips starting from each start station
+trips %>% 
+  group_by(start_station_name, end_station_name, gender) %>% 
+  summarise(count = n()) %>% 
+  arrange(start_station_name, desc(count)) %>% 
+  group_by(start_station_name) %>% 
+  slice_head(n = 3)
 
 # find the top 3 most common station-to-station trips by gender
+trips %>% 
+  group_by(gender, start_station_name, end_station_name) %>% 
+  summarise(count = n()) %>% 
+  arrange(gender, desc(count)) %>% 
+  group_by(gender) %>% 
+  slice_head(n = 3)
 
 # find the day with the most trips
 # tip: first add a column for year/month/day without time of day (use as.Date or floor_date from the lubridate package)
-
+trips %>%
+  mutate(trip_date = as.Date(starttime)) %>%
+  group_by(trip_date) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count)) %>%
+  slice(1)
 # compute the average number of trips taken during each of the 24 hours of the day across the entire month
 # what time(s) of day tend to be peak hour(s)?
+trips %>%
+  mutate(hour = hour(starttime)) %>%
+  group_by(hour) %>%
+  summarise(average_trips = n() / n_distinct(as.Date(starttime))) %>% 
+  arrange(desc(average_trips))
+
--- week1/citibike.sh
+++ week1/citibike.sh
@@ -4,12 +4,14 @@
 #
 
 # count the number of unique stations
-cut -d, -f5 201402-citibike-tripdata.csv | sort | uniq | wc -l
+tail -n +2 201402-citibike-tripdata.csv | cut -d, -f2 | cut -d' ' -f1 | sort | uniq | wc -l
+
 # count the number of unique bikes
 cut -d, -f12 201402-citibike-tripdata.csv | sort | uniq | wc -l
 # count the number of trips per day
 tail -n +2 201402-citibike-tripdata.csv | cut -d, -f2 |cut -d' ' -f1 | uniq -c | sort -n
 cut -d, -f2 201402-citibike-tripdata.csv | cut -d' ' -f1 | uniq -c | sort -n  
+
 # find the day with the most rides
 cut -d, -f2 201402-citibike-tripdata.csv | cut -d' ' -f1 | uniq -c | sort -n | tail -1
 # find the day with the fewest rides
@@ -18,6 +20,30 @@ tail -n +2 201402-citibike-tripdata.csv | cut -d, -f2 |cut -d' ' -f1 | uniq -c |
 cut -d, -f12 201402-citibike-tripdata.csv | sort | uniq -c | sort | tail -1
 # count the number of rides by gender and birth year
 tail -n +2 201402-citibike-tripdata.csv | cut -d, -f14,15 | sort | uniq -c
-# count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)cut -d, -f5 201402-citibike-tripdata.csv | grep '[0-9].*&.*[0-9]'| sort | uniq -c | sort -n
+# count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
+cut -d, -f5 201402-citibike-tripdata.csv | grep '[0-9].*&.*[0-9]'| sort | uniq -c | sort -n | wc -l
 # compute the average trip duration
 awk -F, '{sum += $1; count++} END {if(count>0) print sum / count}' 201402-citibike-tripdata.csv
+# compute the variance of the trip duration
+
+
+# what if the mean is 10^12?
+
+awk -F, '
+{
+    n++
+    delta = $1 - mean
+    mean += delta / n
+    delta2 = $1 - mean
+    M2 += delta * delta2
+} 
+END {
+    if (n > 1) {
+        variance = M2 / (n - 1)
+        print variance
+    }
+}
+' 201402-citibike-tripdata.csv
+
+
+
