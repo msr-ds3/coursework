7175aaab574201d4bd188838eda581c34fd94398
shmandelbaum <shmandelbaum@shana-surface>
2024-06-06 16:45:40 -0400

    Citibike plots

--- week1/plot_trips.R
+++ week1/plot_trips.R
@@ -18,13 +18,49 @@ load('trips.RData')
 ########################################
 
 # plot the distribution of trip times across all rides (compare a histogram vs. a density plot)
-
+trips |> 
+  filter(tripduration <= 3600) |> 
+  ggplot(aes(x = tripduration)) +
+  geom_histogram(bins = 100) +
+  scale_y_continuous(label = comma)
+trips |> 
+  filter(tripduration <= 3600) |> 
+  ggplot(aes(x = tripduration)) +
+  geom_density()
+  
 # plot the distribution of trip times by rider type indicated using color and fill (compare a histogram vs. a density plot)
-
+trips |> 
+  filter(tripduration <= 3600) |> 
+  ggplot(aes(x = tripduration, color = usertype, fill = usertype)) +
+  geom_histogram(bins = 100) +
+  scale_y_continuous(label = comma)
+trips |> 
+  filter(tripduration <= 3600) |> 
+  ggplot(aes(x = tripduration, color = usertype, fill = usertype)) +
+  geom_density() +
+  scale_y_continuous(label = comma)
 # plot the total number of trips on each day in the dataset
-
+trips |> 
+  mutate(days = date(starttime)) |> 
+  group_by(days) |> 
+  summarise(num_of_rides = n()) |> 
+  ggplot(aes(x = days, y = num_of_rides)) +
+  geom_point() +
+  geom_line()
+  
 # plot the total number of trips (on the y axis) by age (on the x axis) and gender (indicated with color)
 
+trips |> 
+  mutate(age = 2014 - birth_year) |> 
+  group_by(age, gender) |> 
+  summarise(num_rides_by_age = n()) |> 
+  ggplot(aes(x = age, y = num_rides_by_age, color = gender)) +
+  geom_point() +
+  coord_cartesian(ylim = c(0,300000)) +
+  scale_y_continuous(labels = comma)
+
+
+  
 # plot the ratio of male to female trips (on the y axis) by age (on the x axis)
 # hint: use the pivot_wider() function to reshape things to make it easier to compute this ratio
 # (you can skip this and come back to it tomorrow if we haven't covered pivot_wider() yet)
@@ -33,8 +69,11 @@ load('trips.RData')
 # plot weather data
 ########################################
 # plot the minimum temperature (on the y axis) over each day (on the x axis)
-
+weather |> 
+  ggplot(aes(x = ymd, y = tmin)) +
+  geom_point()
 # plot the minimum temperature and maximum temperature (on the y axis, with different colors) over each day (on the x axis)
+
 # hint: try using the pivot_longer() function for this to reshape things before plotting
 # (you can skip this and come back to it tomorrow if we haven't covered reshaping data yet)
 
@@ -47,16 +86,80 @@ trips_with_weather <- inner_join(trips, weather, by="ymd")
 
 # plot the number of trips as a function of the minimum temperature, where each point represents a day
 # you'll need to summarize the trips and join to the weather data to do this
+trips_with_weather |> 
+  group_by(ymd) |> 
+  summarise(num_of_trips = n(),
+            min_temp = min(tmin)) |> 
+  ggplot(aes(x = min_temp, y = num_of_trips)) +
+  geom_point()
 
 # repeat this, splitting results by whether there was substantial precipitation or not
 # you'll need to decide what constitutes "substantial precipitation" and create a new T/F column to indicate this
-
+trips_with_weather |> 
+  mutate(sub_prcp = if_else(prcp >= .59, "T", "F")) |> 
+  group_by(ymd, sub_prcp) |> 
+  summarise(num_of_trips = n(),
+            min_temp = min(tmin)) |> 
+  ggplot(aes(x = min_temp, y = num_of_trips, color = sub_prcp)) +
+  geom_point()
+  
 # add a smoothed fit on top of the previous plot, using geom_smooth
-
+trips_with_weather |> 
+  mutate(sub_prcp = if_else(prcp >= .59, "T", "F")) |> 
+  group_by(ymd, sub_prcp) |> 
+  summarise(num_of_trips = n(),
+            min_temp = min(tmin)) |> 
+  ggplot(aes(x = min_temp, y = num_of_trips, color = sub_prcp)) +
+  geom_point() +
+  geom_smooth()
 # compute the average number of trips and standard deviation in number of trips by hour of the day
 # hint: use the hour() function from the lubridate package
+trips_with_weather |> 
+  mutate(hour = hour(starttime)) |>
+  group_by(ymd, hour) |> 
+  summarise(trips_taken = n()) |> 
+  group_by(hour) |> 
+  summarise(avg_trips_taken = mean(trips_taken),
+            sd_trips_taken = sd(trips_taken))
 
-# plot the above
+  # plot the above
+trips_with_weather |> 
+  mutate(hour = hour(starttime)) |>
+  group_by(ymd, hour) |> 
+  summarise(trips_taken = n()) |> 
+  group_by(hour) |> 
+  summarise(avg_trips_taken = mean(trips_taken),
+            sd_trips_taken = sd(trips_taken)) |> 
+  ggplot(aes(x = hour, y = avg_trips_taken)) +
+  geom_point()
 
+trips_with_weather |> 
+  mutate(hour = hour(starttime)) |>
+  group_by(ymd, hour) |> 
+  summarise(trips_taken = n()) |> 
+  group_by(hour) |> 
+  summarise(avg_trips_taken = mean(trips_taken),
+            sd_trips_taken = sd(trips_taken)) |> 
+  ggplot(aes(x = hour, y = sd_trips_taken)) +
+  geom_point()
 # repeat this, but now split the results by day of the week (Monday, Tuesday, ...) or weekday vs. weekend days
 # hint: use the wday() function from the lubridate package
+  trips_with_weather |> 
+    mutate(hour = hour(starttime), day_of_week = wday(starttime)) |>
+    group_by(ymd, hour, day_of_week) |> 
+    summarise(trips_taken = n()) |> 
+    group_by(hour, day_of_week) |> 
+    summarise(avg_trips_taken = mean(trips_taken),
+              sd_trips_taken = sd(trips_taken)) |> 
+    ggplot(aes(x = hour, y = avg_trips_taken, color = as.factor(day_of_week))) +
+    geom_point() 
+  trips_with_weather |> 
+    mutate(hour = hour(starttime), day_of_week = wday(starttime)) |>
+    group_by(ymd, hour, day_of_week) |> 
+    summarise(trips_taken = n()) |> 
+    group_by(hour, day_of_week) |> 
+    summarise(avg_trips_taken = mean(trips_taken),
+              sd_trips_taken = sd(trips_taken)) |> 
+    ggplot(aes(x = hour, y = sd_trips_taken, color = as.factor(day_of_week))) +
+    geom_point()
+
