a01f64e760930fde5107c199b4418ce3bc66f721
root <root@smriti-surface>
2024-06-20 17:12:19 -0400

    week3

--- week3/ngrams/02_filter_1grams.sh
+++ week3/ngrams/02_filter_1grams.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 # filter original 1gram file googlebooks-eng-all-1gram-20120701-1.gz to only lines where the ngram exactly matches a year (18xx, 19xx, or 20xx, where x is a digit)
+
 #   decompress the first using gunzip, zless, zcat or similar
+zcat googlebooks-eng-all-1gram-20120701-1.gz | grep -E '^(18|19|20)[0-9][0-9]\s' > "year_counts.tsv"
+
+
+
 #   then filter out rows that match using grep -E, egrep, awk, or similar
+	
 #   write results to year_counts.tsv
+	
--- week3/ngrams/04_reformat_totals.sh
+++ week3/ngrams/04_reformat_totals.sh
@@ -2,4 +2,6 @@
 
 # reformat total counts in googlebooks-eng-all-totalcounts-20120701.txt to a valid csv
 #   use tr, awk, or sed to convert tabs to newlines
+cat googlebooks-eng-all-totalcounts-20120701.txt | tr -s "\\t" "\\n" > total_counts.csv
+
 #   write results to total_counts.csv
