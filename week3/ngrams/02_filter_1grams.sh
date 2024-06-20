ad754476986b567bfc608ac3d56782a4ecdf4bf4
chukwuebukad <buksdikeocha17@gmail.com>
2024-06-20 16:38:43 -0400

    work until before plotting years to half life

--- week3/ngrams/02_filter_1grams.sh
+++ week3/ngrams/02_filter_1grams.sh
@@ -4,3 +4,5 @@
 #   decompress the first using gunzip, zless, zcat or similar
 #   then filter out rows that match using grep -E, egrep, awk, or similar
 #   write results to year_counts.tsv
+
+zcat googlebooks-eng-all-1gram-20120701-1.gz | grep -E "^(18[0-9]{2}|19[0-9]{2}|20[0-9]{2})\s" > "year_counts.tsv"
