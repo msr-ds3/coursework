8368d9b0582b1a2616e9c810f89eb1936d012461
YinoXu <yx2347@nyu.edu>
2024-06-25 17:17:54 -0400

    Save current progress

---
+++ week4/load_trips_2015.R
@@ -0,0 +1,58 @@
+library(dplyr)
+library(readr)
+
+# define a function to turn strings into datetimes
+parse_datetime <- function(s, format="%Y-%m-%d %H:%M:%S") {
+  as.POSIXct(as.character(s), format=format)
+}
+
+########################################
+# load and clean trip data
+########################################
+
+# load each month of the trip data into one big data frame
+csvs <- Sys.glob('*-tripdata.csv')
+csvs_2 <- Sys.glob('*-tripdata_2.csv')
+all_csvs <- c(csvs, csvs_2)
+
+trips <- data.frame()
+for (csv in all_csvs) {
+  print(csv)
+  tmp <- read_csv(csv, na='\\N')
+  
+  # the date format changed to something ugly in 2014-09 which read_csv doesn't recognize as a datetime,
+  # so manually convert the date from a string to a datetime
+  if (typeof(tmp$starttime) == "character")
+    tmp <- mutate(tmp,
+                  starttime=parse_datetime(starttime, "%m/%d/%Y %H:%M"),
+                  stoptime=parse_datetime(stoptime, "%m/%d/%Y %H:%M"))
+  
+  trips <- rbind(trips, tmp)
+}
+
+# replace spaces in column names with underscores
+names(trips) <- gsub(' ', '_', names(trips))
+
+# add a column for year/month/day (without time of day)
+trips <- mutate(trips, ymd=as.Date(starttime))
+
+# recode gender as a factor 0->"Unknown", 1->"Male", 2->"Female"
+trips <- mutate(trips, gender=factor(gender, levels=c(0,1,2), labels=c("Unknown","Male","Female")))
+
+########################################
+# load and clean weather data
+########################################
+
+# load weather data from belvedere tower in central park
+weather <- read_csv('weather_2015.csv')
+
+# extract just a few columns, lowercase column names, and parse dates
+weather <- select(weather, DATE, PRCP, SNWD, SNOW, TMAX, TMIN)
+names(weather) <- tolower(names(weather))
+weather <- mutate(weather,
+                  ymd = as.Date(parse_datetime(date, "%Y-%m-%d")))
+weather <- tbl_df(weather)
+
+# save data frame for easy loading in the future
+save(trips, weather, file='trips_2015.RData')
+
