a602a44fcb00bf484ad1fdcd8a83ce023f29ec2b
naomi <nbeck1@mail.yu.edu>
2025-05-28 14:44:54 -0400

    assignemnt2

--- week1/citibike.sh
+++ week1/citibike.sh
@@ -21,10 +21,13 @@ cat 201402-citibike-tripdata.csv |cut -d,  -f2 | grep -o '^.* ' | sort | uniq -c
 
 
 # find the id of the bike with the most rides
+cat 201402-citibike-tripdata.csv |cut -d,  -f12 | sort | uniq -c | sort -r | head -n1 | awk '{print $2}'
 
 # count the number of rides by gender and birth year
+cat 201402-citibike-tripdata.csv | cut -d, -f 14,15 | sort | uniq | wc -l
 
 # count the number of trips that start on cross streets that both contain numbers (e.g., "1 Ave & E 15 St", "E 39 St & 2 Ave", ...)
-
+cat 201402-citibike-tripdata.csv | cut -d, -f5 | grep '.*[0-9]+*.*&.*[0-9]+*' | wc -l
 
 # compute the average trip duration
+cat 201402-citibike-tripdata.csv | cut -d, -f1 | awk -F, '{sum += $1; count +=1} END {print sum/ count}'
