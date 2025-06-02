3476cb3a4e127a1ffb0dc067a2e58944db534efa
akone42 <konealoubadra@gmail.com>
2025-06-02 10:39:18 -0400

    Day 4 plotting update

--- week1/plot_trips.R
+++ week1/plot_trips.R
@@ -47,12 +47,11 @@ ggplot(aes(x = date))+
     scale_x_date(date_labels = "%Y-%m-%d")
     
 # plot the total number of trips (on the y axis) by age (on the x axis) and gender (indicated with color)
-trips %>% mutate(age = sys.Date() - birth_year) %>%
-ggplot(aes(x = age, color = usertype, fill = usertype))+
-    geom_histogram(bins = 30) + 
-    scale_x_log10(labels=comma) +
-    ylab()+
-    facet_grid(~usertype)
+trips %>% mutate(age = as.numeric(format(ymd, '%Y')) - as.numeric(birth_year)) %>%
+    ggplot(aes(x = age), fill = 'gender')+
+    geom_histogram(bins = 30)
+
+
 # plot the ratio of male to female trips (on the y axis) by age (on the x axis)
 # hint: use the pivot_wider() function to reshape things to make it easier to compute this ratio
 # (you can skip this and come back to it tomorrow if we haven't covered pivot_wider() yet)
@@ -61,6 +60,8 @@ ggplot(aes(x = age, color = usertype, fill = usertype))+
 # plot weather data
 ########################################
 # plot the minimum temperature (on the y axis) over each day (on the x axis)
+ggplot(weather, aes(x =date, y = tmin)) +
+        geom_point()
 
 # plot the minimum temperature and maximum temperature (on the y axis, with different colors) over each day (on the x axis)
 # hint: try using the pivot_longer() function for this to reshape things before plotting
@@ -71,15 +72,48 @@ ggplot(aes(x = age, color = usertype, fill = usertype))+
 ########################################
 
 # join trips and weather
-
+trips_with_weather <- inner_join(trips, weather, by = "ymd")
 
 # plot the number of trips as a function of the minimum temperature, where each point represents a day
 # you'll need to summarize the trips and join to the weather data to do this
 
+trips_with_weather %>% 
+    group_by(ymd,tmin) %>% 
+         summarise(count = n(), .groups = "drop") %>% 
+            ggplot(aes(x = tmin, y = count)) +
+                geom_point() +
+                ylab("minimum temperature") +
+                xlab("number of trip")
+
 # repeat this, splitting results by whether there was substantial precipitation or not
 # you'll need to decide what constitutes "substantial precipitation" and create a new T/F column to indicate this
 
+trips_with_weather %>% 
+    mutate(prcp_T_F = ifelse(prcp == 0, 'F', 'T')) %>% 
+    group_by(ymd,tmin,prcp_T_F) %>% 
+         summarise(count = n(),.groups = "drop") %>% 
+         ungroup() %>% 
+            ggplot(aes(x = tmin, y = count)) +
+                geom_point() +
+                ylab("minimum temperature") +
+                xlab("number of trip") +
+                facet_wrap(~prcp_T_F)
+
 # add a smoothed fit on top of the previous plot, using geom_smooth
+
+
+
+trips_with_weather %>% 
+    mutate(prcp_T_F = ifelse(prcp > mean(prcp), 'T', 'F')) %>% 
+    group_by(ymd,tmin,prcp_T_F) %>% 
+         summarise(count = n(),.groups = "drop") %>% 
+         ungroup() %>% 
+            ggplot(aes(x = tmin, y = count)) +
+                geom_point() +
+                geom_smooth() +
+                ylab("minimum temperature") +
+                xlab("number of trip") +
+                facet_wrap(~prcp_T_F)
 
 # compute the average number of trips and standard deviation in number of trips by hour of the day
 # hint: use the hour() function from the lubridate package
