a62ebf650530c9f8d76d445886bc7f8a4c005ae4
Jake Hofman <jhofman@gmail.com>
2025-06-13 10:29:51 -0400

    ngrams

---
+++ week3/ngrams/04_reformat_totals.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# reformat total counts in googlebooks-eng-all-totalcounts-20120701.txt to a valid csv
+#   use tr, awk, or sed to convert tabs to newlines
+#   write results to total_counts.csv
