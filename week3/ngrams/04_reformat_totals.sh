49b414b5ca66545292cd80c2e34d2adad6adf3a1
SofCora <scora@fordham.edu>
2025-06-16 10:30:08 -0400

    solutions from friday up to but not including the inlet graph

--- week3/ngrams/04_reformat_totals.sh
+++ week3/ngrams/04_reformat_totals.sh
@@ -3,3 +3,8 @@
 # reformat total counts in googlebooks-eng-all-totalcounts-20120701.txt to a valid csv
 #   use tr, awk, or sed to convert tabs to newlines
 #   write results to total_counts.csv
+
+tr '\t' '\n' < googlebooks-eng-all-totalcounts-20120701.txt > total_counts.csv
+#[ -f ratings.csv ] || cat ratings.dat | sed 's/::/,/g' > ratings.csv
+
+#[ -f movies.tsv ] || cat movies.dat | sed 's/::/	/g' > movies.tsv
