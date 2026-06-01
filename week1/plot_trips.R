eff92decf194802a5ca1a7505f6ae6080681aca2
faridabalogun <faridabalogun1217@gmail.com>
2026-06-01 00:46:48 -0400

    Plot Assignment 3

--- week1/plot_trips.R
+++ week1/plot_trips.R
@@ -18,12 +18,20 @@ load('trips.RData')
 ########################################
 
 # plot the distribution of trip times across all rides (compare a histogram vs. a density plot)
-
+trips %>%ggplot(aes(x=tripduration)) + geom_histogram()+scale_x_log10(label = comma )
+ggplot(trips,aes(x=tripduration))+ geom_density(fill = "grey"  )+scale_x_log10(label = comma )
 # plot the distribution of trip times by rider type indicated using color and fill (compare a histogram vs. a density plot)
+ggplot(trips, aes(x = tripduration  , color = usertype, fill =  usertype ))  + geom_histogram() +scale_x_log10(label = comma )
+ggplot(trips, aes(x = tripduration  , color = usertype, fill =  usertype ))  + geom_density() +scale_x_log10(label = comma )
 
 # plot the total number of trips on each day in the dataset
-
+trips %>%
+mutate(day = floor_date(starttime,unit="day"))%>%group_by(day)%>%summarize(count = n())%>% ggplot(aes(x = day,y = count)) + geom_line()
 # plot the total number of trips (on the y axis) by age (on the x axis) and gender (indicated with color)
+trips %>%
+group_by(birth_year,gender)%>%
+summarize(count = n())%>%group_by(gender)%>%ggplot(aes(x = birth_year, y = count, fill = gender, color = gender))+ geom_line() +scale_y_log10(label = comma )
+
 
 # plot the ratio of male to female trips (on the y axis) by age (on the x axis)
 # hint: use the pivot_wider() function to reshape things to make it easier to compute this ratio
@@ -33,7 +41,7 @@ load('trips.RData')
 # plot weather data
 ########################################
 # plot the minimum temperature (on the y axis) over each day (on the x axis)
-
+weather%>%mutate(day = floor_date(ymd,unit="day"))%>%ggplot(aes(x = day , y = tmin))+ geom_point()  +scale_y_log10(label = comma )
 # plot the minimum temperature and maximum temperature (on the y axis, with different colors) over each day (on the x axis)
 # hint: try using the pivot_longer() function for this to reshape things before plotting
 # (you can skip this and come back to it tomorrow if we haven't covered reshaping data yet)
@@ -47,16 +55,27 @@ trips_with_weather <- inner_join(trips, weather, by="ymd")
 
 # plot the number of trips as a function of the minimum temperature, where each point represents a day
 # you'll need to summarize the trips and join to the weather data to do this
-
+trips_with_weather%>%group_by(ymd)%>%summarize(count=n(), mean = mean(tmin))%>% ggplot(aes(x=mean, y = count))
+ +geom_point()
 # repeat this, splitting results by whether there was substantial precipitation or not
 # you'll need to decide what constitutes "substantial precipitation" and create a new T/F column to indicate this
+trips_with_weather%>% mutate(prcptf = ifelse (prcp > mean(prcp) , "T", "F")) %>% group_by(ymd,prcptf)%>%
+summarize(count=n(), mean = mean(tmin))%>% 
+ggplot(aes(x=mean, y = count)) +geom_point()
+#+facet_wrap(~prcptf)
 
 # add a smoothed fit on top of the previous plot, using geom_smooth
-
+trips_with_weather%>% mutate(prcptf = ifelse (prcp > mean(prcp) , "T", "F")) %>% group_by(ymd,prcptf)%>%
+summarize(count=n(), mean = mean(tmin))%>% 
+ggplot(aes(x=mean, y = count)) +geom_point()+geom_smooth(method = "lm")
 # compute the average number of trips and standard deviation in number of trips by hour of the day
 # hint: use the hour() function from the lubridate package
-
+trips_with_weather %>% mutate(hour = hour(starttime),day = floor_date(ymd,unit="day"))%>% 
+group_by(day,hour) %>% summarize (count = n())%>% 
+group_by(hour)%>%summarize (mean = mean(count),sd = sd(count))
 # plot the above
-
+trips_with_weather %>% mutate(hour = hour(starttime),day = floor_date(ymd,unit="day"))%>% 
+group_by(day,hour) %>% summarize (count = n())%>% 
+group_by(hour)%>%summarize (mean = mean(count),sd = sd(count))%>%ggplot(aes(x=sd,y = mean))+geom_line()
 # repeat this, but now split the results by day of the week (Monday, Tuesday, ...) or weekday vs. weekend days
 # hint: use the wday() function from the lubridate package
