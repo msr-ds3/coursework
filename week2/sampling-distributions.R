c2d87ddab7689def1ec562fece133fae7d9e390a
Addina <addinarahaman0@gmail.com>
2026-06-02 16:08:04 -0400

    Resolve commit error

--- week2/sampling-distributions.R
+++ week2/sampling-distributions.R
@@ -76,22 +76,33 @@ sd
 # is tossed three times. If all tosses end up "Head" then the player wins
 # $10. Otherwise, the player loses the investment.
 #
-values <- c(0, 1)
+
+values <- c(0,1)
 probs <- c(1/2, 1/2)
 
 # 1. What is the probability of winning the game?
 # 1/2 * 1/2 * 1/2 = 1/8
 (probs[values == 1])^3
 
+# another way to do it
+dbinom(3, 3, 1/2)
+
 # 2. What is the probability of losing the game?
 # 1 - 1/8 = 7/8
 1 - (probs[values == 1])^3
+
+# another way to do it
+1 - dbinom(3, 3, 1/2)
 
 # 3. What is the expected gain for the player that plays this game?
 #    (Notice that the expectation can obtain a negative value.)
 # A player can gain $8 and lose $2. This can be represented by 8 and -2.
 # 8 * 1/8 + -2 * 7/8 = 1 - 7/4 = -3/4
 8 * (probs[values == 1])^3 + (-2) * (1 - (probs[values == 1])^3)
+
+# another way
+8 * (dbinom(3, 3, 1/2)) + (-2) * (1 - dbinom(3, 3, 1/2))
+
 
 
 ####################################################################################
@@ -109,16 +120,34 @@ probs <- c(1/2, 1/2)
 # Normal distribution with a mean of 630kg and a standard deviation of 61kg.
 #
 # 1. What is the probability that the total weight of 8 people exceeds 650kg?
-mu <- 560
-sd <- 57
-qnorm(50, mu, sd)
+mu_8 <- 560
+sd_8 <- 57
+1 - pnorm(650, mu_8, sd_8)
+# The probability is 0.05717406 or ~5%
 
 # 2. What is the probability that the total weight of 9 people exceeds 650kg?
+mu_9 <- 630
+sd_9 <- 61
+1 - pnorm(650, mu_9, sd_9)
+# The probability is 0.3715054 or ~37%
+
 # 3. What is the central region that contains 80% of distribution of the
 #    total weight of 8 people?
+# This would be everyone between 10% and 90%
+tenth_percentile_8 <- qnorm(0.1, mu_8, sd_8)
+ninetieth_percentile_8 <- qnorm(0.9, mu_8, sd_8)
+# The interval can be expressed as 
+c(tenth_percentile_8, ninetieth_percentile_8)
+# This is the total weight interval [486.9516, 633.0484]
+
 # 4. What is the central region that contains 80% of distribution of the
 #    total weight of 9 people?
+tenth_percentile_9 <- qnorm(0.1, mu_9, sd_9)
+ninetieth_percentile_9 <- qnorm(0.9, mu_9, sd_9)
+# The interval can be expressed as 
+c(tenth_percentile_9, ninetieth_percentile_9)
 
+# This is the total weight interval [551.8254, 708.1746]
 # Hint: use pnorm() and qnorm().
 
 
@@ -135,19 +164,61 @@ qnorm(50, mu, sd)
 # Our goal is to investigate the sampling distribution of the sample average
 # of the variable "bmi". We assume a sample of size n = 150.
 #
+pop2 <- read_csv("http://pluto.huji.ac.il/~msby/StatThink/Datasets/pop2.csv")
 # 1. Compute the population average of the variable "bmi".
+bmi_mu <- pop2 %>%
+    select(bmi) %>%
+    summarize(avg_bmi=mean(bmi)) %>%
+    pull()
+bmi_mu
+# The average population bmi is 24.98446
+
+
 # 2. Compute the population standard deviation of the variable "bmi".
+bmi_sd <- pop2 %>%
+    select(bmi) %>%
+    summarize(sd_bmi=sd(bmi)) %>%
+    pull()
+bmi_sd
+# The population standard deviation is 4.188511
+
 # 3. Compute the expectation of the sampling distribution for the sample
 #    average of the variable.
+# The expectation should theoretically be the mean 24.98446.
+
 # 4. Compute the standard deviation of the sampling distribution for the
 #    sample average of the variable.
+# The standard deviation should be the standard error.
+# This is sd/sqrt(N)
+bmi_se <- bmi_sd/sqrt(150)
+bmi_se
+
 # 5. Identify, using simulations, the central region that contains 80% of
 #    the sampling distribution of the sample average.
+
+n = 150
+
+# replicate a pipe 1e5 times
+mu_hat <- replicate(1e5, pop2 %>%
+    select(bmi) %>%
+    slice_sample(n=n) %>%
+    summarize(mean(bmi)) %>%
+    pull())
+
+quantile(mu_hat, c(0.1, 0.9))
+# This should print
+#     10%      90% 
+# 24.46307 25.57535
+
 # 6. Identify, using the Central Limit Theorem, an approximation of the
 #    central region that contains 80% of the sampling distribution of the
 #    sample average.
 
-pop2 <- read_csv("http://pluto.huji.ac.il/~msby/StatThink/Datasets/pop2.csv")
+tenth_percentile = qnorm(0.1, bmi_mu, bmi_se)
+ninetieth_percentile = qnorm(0.9, bmi_mu, bmi_se)
+# This can be expressed as
+c(tenth_percentile, ninetieth_percentile)
+# This is [24.54618, 25.42274]
 
 # Hint: for (5), use replicate() to draw many samples of size 150,
 # compute the mean of bmi for each, then use quantile().
