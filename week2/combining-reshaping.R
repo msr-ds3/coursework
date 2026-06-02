27ce70c33579a9e0df39b91d738ce8b9ff5e848b
Ebeene01 <manebabou01@gmail.com>
2026-06-02 15:55:43 -0400

    Day5 Assignment Completed

--- week2/combining-reshaping.R
+++ week2/combining-reshaping.R
@@ -8,10 +8,26 @@ library(tidyverse)
 #   2. Extract the matching population per country per year.
 #   3. Divide cases by population, and multiply by 10000.
 #   4. Store back in the appropriate place.
+table2%>%
+  group_by(year)%>%
+  pivot_wider(names_from = type, values_from = count)%>%
+  mutate(rate = cases / population * 10000)
+
+taba <- table4a%>%
+  pivot_longer(names_to = "year", values_to = "case", -country)
+tabb <- table4b%>%
+  pivot_longer(names_to = "year", values_to = "population", -country)
+left_join(taba, tabb, by = c("country", "year"))%>%
+  mutate(rate = case / population * 10000)
 
 # Which representation is easiest to work with? Which is hardest? Why?
 # Add your answer as a comment.
 
+# Working with these two tables, I personnaly think that the table2 was the easiest to work with
+# because every country is already separated based on the year, type,and count. Matching the cases, and population,
+# is much easier after we group the countries yearly. However, the two table4 are the hardest to work with 
+# because the values that we need are separated in two different tables and we need to combine them back to be able to use them.
+# Basically, the table4 isn't that hard to work with, it just requires more steps and a deeper analysis.
 
 ####################################################################################
 # 12.3.3 Exercise 1
@@ -30,6 +46,13 @@ stocks %>%
 # pivot_longer() has a names_ptypes argument, e.g.  names_ptypes = list(year = double()). 
 # What does it do? Add your answer as a comment.
 
+# I think pivot_longer() and pivot_wider() aren't perfectly symmetrical 
+# because when you pivot you change the nature of the data like the columns,
+# When we pivot_wider() from the original data frame, each colum contains the half
+# and return by corresponding year, which is provided column wise. It is kind of a confusing dataset.
+# pivot_longer() defines the class, attributes, and types of a vector. It's mostly used 
+# to be sure that the columns we've created are what we expected. It's not to change the given table. 
+
 ####################################################################################
 # 12.3.3 Exercise 3
 # What would happen if you widen this table? Why? 
@@ -44,3 +67,11 @@ people <- tribble(
   "Jessica Cordero", "age",       37,
   "Jessica Cordero", "height",   156
 )
+
+# This won't be balanced if we widen the table because we have two "ages" for Phillip with one "height" only
+# and only one "age" and "height" for Jessica.
+
+# people%>%
+#   group_by(name, names)%>%
+#   mutate(nouv=row_number())%>%
+#   pivot_wider(names_from = name, values_from = values)
---
+++ week2/diamond-sizes.md
@@ -0,0 +1,28 @@
+---
+title: "Diamond sizes"
+date: 2016-08-25
+output: html_document
+---
+
+
+
+We have data about 53940 diamonds. Only 
+126 are larger than
+2.5 carats. The distribution of the remainder is shown
+below:
+
+![plot of chunk unnamed-chunk-1](figure/unnamed-chunk-1-1.png)
+
+---
+27.3.1 Exercise 3
+---
+---
+TODO
+---
+
+---
+27.4.7 Exercise 1
+---
+---
+TODO
+---
