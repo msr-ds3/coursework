38f9d5b85472fc540f87fa459067d34eeff44775
Jacqueline Louie <jacquelineyylouie@gmail.com>
2026-06-02 23:41:25 -0400

    Adjusted plot_trips per Hannah suggestions

--- week1/plot_trips.R
+++ week1/plot_trips.R
@@ -18,34 +18,47 @@ load('trips.RData')
 ########################################
 
 # plot the distribution of trip times across all rides (compare a histogram vs. a density plot)
-ggplot(trips, aes(x=tripduration)) +
-    scale_x_log10(label = comma) +
-    geom_histogram(bins = 200) + 
+# Changed plots per Hannah suggestions
+smaller_trips <- trips %>%
+    filter(tripduration <= 5000)
+
+ggplot(smaller_trips, aes(x=tripduration)) +
+    geom_histogram(bins = 100) + 
     labs(x = "Trip Duration", x = "Trip Count", title = "Distribution of Trip Time Across All Rides")
 
-ggplot(trips, aes(x=tripduration)) +
-    scale_x_log10(label = comma) +
+ggplot(smaller_trips, aes(x=tripduration)) +
     geom_density(fill = "#bbbbf8", colour = "#bbbbf8") +
     labs(x = "Trip Duration", y = "Trip Count", title = "Distribution of Trip Times Across All Rides")
 
+# CHANGES: I removed the log10 scale and instead filtered out extreme values
+
+
 # plot the distribution of trip times by rider type indicated using color and fill (compare a histogram vs. a density plot)
-ggplot(trips, aes(x=tripduration, colour=usertype, fill=usertype)) +
+# Changed plots per Hannah suggestions
+ggplot(smaller_trips, aes(x=tripduration, colour=usertype, fill=usertype)) +
     scale_x_log10(label = comma) +
     geom_histogram(bins = 50) +
-    facet_wrap(~ usertype) +
+    facet_wrap(~ usertype, scale = "free_y") +
     labs(x = "Trip Duration", y = "Trip Count", title = "Distribution of Trip Times Across All Rides (Split by User Type)")
 
+#
 ggplot(trips, aes(x=tripduration, colour=usertype, fill=usertype)) +
     scale_x_log10(label = comma) +
     geom_density() +
     labs(x = "Trip Duration", y = "Density", title = "Distribution of Trip Times Across All Rides (Colored by User Type)")
 
 # plot the total number of trips on each day in the dataset
+# Changed plot from bar plot to line graph per Hannah suggestion
 trips %>%
     mutate(day = floor_date(starttime, unit = "day")) %>%
-    ggplot(aes(x=day)) +
-    geom_bar(fill = '#9797c0') +
+    group_by(day) %>%
+    summarize(count = n()) %>%
+    ggplot(aes(x=day, y = count)) +
+    geom_line(colour = '#9797c0') +
     labs(x = "Day", y = "Trip Count", title = "Total number of trips per day")
+
+# If you want to see changes across days, it is more difficult to follow if you are 
+# looking a barplot as opposed to a line graph
 
 # plot the total number of trips (on the y axis) by age (on the x axis) and gender (indicated with color)
 trips %>%
@@ -54,12 +67,15 @@ trips %>%
     geom_bar(alpha = 1/5, position = "identity") +
     labs(x = "Age", y = "Total number of trips", title = "Total number of trips vs age (colored by gender)")
 
-# Scatter plot version (maybe it makes sense? not completely sure)
+# Scatter plot version (Scatter plot version is much better)
+# Kept scatter plot version because it is easier to see the differences betweeen groups
+# Also limited the range to 250000 so the group difference is easier to see
 trips %>%
     mutate(age = 2014 - birth_year) %>%
     group_by(age, gender) %>%
     summarize(count = n()) %>%
     ggplot(aes(x = age, y = count, colour = gender)) +
+    ylim(0, 250000) +
     geom_point() +
     geom_smooth() +
     labs(x = "Age", y = "Total number of trips", title = "Total number of trips vs age (colored by gender)")
@@ -83,7 +99,8 @@ trips %>%
 # plot weather data
 ########################################
 # plot the minimum temperature (on the y axis) over each day (on the x axis)
-ggplot(weather, aes(x = date, y = tmin)) +
+# Made x = year month day for easier visuals on the x-axis per Hannah suggestion
+ggplot(weather, aes(x = ymd(date), y = tmin)) +
     geom_point(colour='#618695') +
     labs(x = "Date", y = "Minimum Temperature", title = "Date vs Minimum temperature")
 
@@ -175,6 +192,8 @@ trips_with_weather %>%
     view() 
 
 # plot the above
+# removed coord-flip, the hours vs count makes more sense the other way around
+# per Hannah suggestion
 trips_with_weather %>%
     mutate(hour = hour(starttime)) %>%
     group_by(date, hour) %>%
@@ -183,18 +202,19 @@ trips_with_weather %>%
     summarize(avg = mean(count), sd = sd(count)) %>%
     ggplot(aes(x = hour, y = avg)) +
     geom_pointrange(aes(ymin = avg - sd, ymax = avg + sd)) +
-    coord_flip() +
     labs(x = "Hour of Day", y = "Average Trips", title = "Average Trips by Hour of Day")
 
 # repeat this, but now split the results by day of the week (Monday, Tuesday, ...) or weekday vs. weekend days
 # hint: use the wday() function from the lubridate package
+# Changed to facet_wrap by day_of_week rather than avg and sd by day_of_week
+# per Hannah suggestion
 trips %>%
-    mutate(date = as.Date(starttime), day_of_week = wday(date)) %>%
-    group_by(date, day_of_week) %>%    
+    mutate(hour = hour(starttime), date = as.Date(starttime), day_of_week = wday(date)) %>%
+    group_by(date, hour, day_of_week) %>%
     summarize(count=n()) %>%
-    group_by(day_of_week) %>%
+    group_by(hour, day_of_week) %>%
     summarize(avg = mean(count), sd = sd(count)) %>%
-    ggplot(aes(x = day_of_week, y = avg)) +
+    ggplot(aes(x = hour, y = avg)) +
     geom_pointrange(aes(ymin = avg - sd, ymax = avg + sd)) +
-    coord_flip() +
-    labs(x = "Day Of Week", y = "Average Trips", title = "Average Trips by Day of Week")
+    facet_wrap(~ day_of_week) +
+    labs(x = "Hour of Day", y = "Average Trips", title = "Average Trips by Hour of Day")
