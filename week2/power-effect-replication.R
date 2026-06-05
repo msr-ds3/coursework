0f696d8fbc4a8d2b5baa8388fab193100dadf907
Ebeene01 <manebabou01@gmail.com>
2026-06-05 15:05:32 -0400

    Assignment Completed

--- week2/power-effect-replication.R
+++ week2/power-effect-replication.R
@@ -26,9 +26,18 @@ magnets <- read_csv("http://pluto.huji.ac.il/~msby/StatThink/Datasets/magnets.cs
 #   expected value of X, the null hypothesis and the alternative hypothesis
 #   for a statistical test to determine the presence of a placebo effect. The null
 #   hypothesis should reflect the situation that the placebo effect is absent
+ # Null hypothesis: E(X) = 0
+ # Alternative hypothesis: E(X) != 0
+
 # 2. Identify the observations that can be used in order to test the hypotheses.
+magnets$change[30:50]
+
 # 3. Carry out the test and report your conclusion. (Use a significance level of
 #    5%.)
+ t.test(magnets$change[30:50], sig.level=0.05)
+ # p-value = 0.004702 < 0.05
+ # Therefore, we reject the null hypothesis, 
+ # meaning that a "placebo effect" seems to be present.
 
 ####################################################################################
 # IST Chapter 13, Exercise 13.1
@@ -50,11 +59,40 @@ magnets <- read_csv("http://pluto.huji.ac.il/~msby/StatThink/Datasets/magnets.cs
 # All tests should conducted at the 5% significance level:
 # 1. Is there a significance difference between the treatment and the control
 #    groups in the expectation of the reported score of pain before the application of the device?
+ 
+ # magnets$score1 --> the scores of pain before a device is applied
+ # ~ --> score1 as a function of active group
+ # magnets$active --> factor indicating which level each pain belongs to
+ t.test(magnets$score1 ~ magnets$active, sig.level=0.05)
+ # p-value = 0.6806 > 0.05
+ # Therefore, we can't reject the null hypothesis that the expectations
+ # in the two groups are equal. There's no significant difference between the treatment
+ # and the control groups.
+
 # 2. Is there a significance difference between the treatment and the control
 #    groups in the variance of the reported score of pain before the application
 #    of the device?
+ var.test(magnets$score1 ~ magnets$active, sig.level=0.05)
+ # p-value = 0.3687 > 0.05
+ # Therefore, we can't reject the null hypothesis that the variances
+ # in the two groups are equal. There's no significant difference between the treatment
+ # and the control groups in the variance of the reported score of pain.
+
 # 3. Is there a significance difference between the treatment and the control
 #    groups in the expectation of the change in score that resulted from the
 #    application of the device?
+ t.test(magnets$change ~ magnets$active, sig.level=0.05)
+ # p-value = 3.86e-07 < 0.05
+ # Therefore, we reject the null hypothesis that the expectation of the change
+ # in the two groups are equal. 
+ # There's a significant difference between the treatment and the control groups.
+ # According to this, magnets have an effect on the expectation of the response 
+
 # 4. Is there a significance difference between the treatment and the control
 #    groups in the variance of the change in score that resulted from the application of the device?
+ var.test(magnets$change ~ magnets$active, sig.level=0.05)
+ # p-value = 0.001535 < 0.05
+ # Therefore, we reject the null hypothesis that the variances in the two groups are equal. 
+ # There's a significant difference between the treatment and the control groups 
+ # in the variance of the reported score of pain.
+ # According to this, magnets also have an effect on the variance of the response 
