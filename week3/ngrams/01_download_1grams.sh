cb0a8e6bfa7acd5b906fa4ed4b877bfd697ed030
Addina <addinarahaman0@gmail.com>
2026-06-11 14:18:17 -0400

    Completed Week 3 Thursday Assignment

--- week3/ngrams/01_download_1grams.sh
+++ week3/ngrams/01_download_1grams.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # use curl or wget to download the version 2 1gram file with all terms starting with "1", googlebooks-eng-all-1gram-20120701-1.gz
+curl -O http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-1gram-20120701-1.gz
 
 # update the timestamp on the resulting file using touch
 # do not remove, this will keep make happy and avoid re-downloading of the data once you have it
