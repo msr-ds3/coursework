3c3eaa5be06b48d1627863e98d1373acce817c5b
alexie-louise <alexiedacosta@yahoo.com>
2026-06-11 10:43:48 -0400

    download_movieslens.sh revised

--- week3/download_movielens.sh
+++ week3/download_movielens.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+
 #
 # downloads movielens rating data
 #
 # see http://grouplens.org/datasets/movielens/ for more info
-#
 
-url=http://files.grouplens.org/datasets/movielens/ml-10m.zip
+
+url=https://files.grouplens.org/datasets/movielens/ml-10m.zip
 
 # download ratings zip file
-[ -f movielens_10M.zip ] || curl -o movielens_10M.zip $url
+[ -f ml_10M.zip ] || curl -o ml_10M.zip $url
 
 # uncompress zip file
 if [ ! -f ratings.dat ]
     then
-    unzip movielens_10M.zip && mv ml-10M100K/* .
+    unzip ml_10M.zip && mv ml-10M100K/* .
 fi
 
 # reformat to comma-separated file
