a83c9dee395cf43c4096f7c596be58aa05a48853
Jacqueline Louie <jacquelineyylouie@gmail.com>
2026-06-11 14:40:59 -0400

    work so far on ngrams

--- week3/ngrams/02_filter_1grams.sh
+++ week3/ngrams/02_filter_1grams.sh
@@ -4,3 +4,6 @@
 #   decompress the first using gunzip, zless, zcat or similar
 #   then filter out rows that match using grep -E, egrep, awk, or similar
 #   write results to year_counts.tsv
+
+zcat googlebooks-eng-all-1gram-20120701-1.gz |
+    awk '/^18[0-9]{2}\t|^19[0-9]{2}\t|^20[0-9]{2}\t/ {print}' > year_counts.tsv
