b45b06f01e037caa432dcf3bdc23b899edcd0eae
ChanaRapp <crapp1@mail.yu.edu>
2026-06-15 13:57:50 -0400

    Completed predict_citibike and ngrams

--- week3/ngrams/01_download_1grams.sh
+++ week3/ngrams/01_download_1grams.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 # use curl or wget to download the version 2 1gram file with all terms starting with "1", googlebooks-eng-all-1gram-20120701-1.gz
+url=http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-1gram-20120701-1.gz
+
+[ -f googlebooks-eng-all-1gram-20120701-1.gz ] || curl --ssl-no-revoke -o googlebooks-eng-all-1gram-20120701-1.gz $url
+
+
+gunzip googlebooks-eng-all-1gram-20120701-1.gz
+
 
 # update the timestamp on the resulting file using touch
 # do not remove, this will keep make happy and avoid re-downloading of the data once you have it
